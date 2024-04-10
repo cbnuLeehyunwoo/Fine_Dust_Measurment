ce987049f6e21005a4da0b4dca64d9ae8a10dfa2
choigowon <cape326@chungbuk.ac.kr>
2024-04-10 17:19:24 +0900

    si

--- scr.py
+++ scr.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_dust():
+def scrape_naver():
     print("[네이버 미세먼지 지수]")
     url="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80&oquery=%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80&tqi=imv3ysqpsECss5%2FFWQdssssstjs-136535"
     res=requests.get(url)
@@ -12,11 +12,24 @@ def scrape_dust():
     dust=soup.find("div", attrs={"class":"tb_scroll"}).find("tbody").find("tr") # 도 별 정보
     place=dust.find("th") # 도 이름
     value=dust.find("span") # 미세먼지 값
-    
+
     print(dust.get_text())
     print(place.get_text())
     print(value.get_text())
 
+def scrape_naver_gang():
+    print("[네이버 강원도 미세먼지 지수]")
+    url="https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blQ3&qvt=0&query=%EA%B0%95%EC%9B%90%20%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80"
+    res=requests.get(url)
+    res.raise_for_status()
+    soup=BeautifulSoup(res.text, "lxml")  
+
+    dust=soup.find("div", attrs={"class":"map_area ct01"})
+    place=dust.find("span", attrs={"class":"cityname"})
+    value=dust.find("span", attrs={"class":"value"}).find("em")
+    print(place.get_text()) # 시 이름
+    print(value.get_text()) # 미세먼지 값
+
 
 if __name__ == "__main__":
-    scrape_dust()
+    scrape_naver_gang()
