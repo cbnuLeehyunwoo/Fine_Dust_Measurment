ae3864f9ea7d6e9b49f8fd0ee2d7a7d0f200d4ee
cbnuLeehyunwoo <henow123@naver.com>
2024-04-12 22:23:37 +0900

    add read_Dust.ino

---
+++ read_Dust.ino
@@ -0,0 +1,33 @@
+
+
+void setup() {
+  // pinMode 및 Serial 초기화
+  Serial.begin(9600);
+  pinMode(PIN_DUST_LED, OUTPUT);
+
+  digitalWrite(PIN_DUST_LED, LOW);  // 미세먼지 센서 적외선 LED 초기값 설정(LOW -> 센서 끔)
+
+}
+
+float read_dust() {
+  digitalWrite(PIN_DUST_LED, HIGH); // (HIGH-> 센서 킴, 데이터 샘플링 시작)
+  delayMicroseconds(280);
+  int dustval = analogRead(PIN_DUST_OUT);
+  delayMicroseconds(40);
+  digitalWrite(PIN_DUST_LED, LOW); // (데이터 샘플링 종료, 센서 끔)
+  delayMicroseconds(9680);
+  return dustval
+}
+
+void print_Serial() {
+  float dust= read_dust();                  // 미세먼지 데이터 수집
+  
+  Serial.print(" %\nDust: ");
+  Serial.print(dust);
+  Serial.println(" ug");
+}
+
+void loop() {
+  read_dust();
+  delay(3000);
+}
