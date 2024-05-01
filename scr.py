61561746ed4e651d9f73bebace8c956266e2a749
choigowon <cape326@chungbuk.ac.kr>
2024-05-01 14:48:43 +0900

    weatheri

--- scr.py
+++ scr.py
@@ -1,44 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-def scrape_naver():
-    print("[네이버 미세먼지 지수]")
-    url="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80&oquery=%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80&tqi=imv3ysqpsECss5%2FFWQdssssstjs-136535"
-    res=requests.get(url)
-    res.raise_for_status() # 문제 발견 시 프로그램 종료
-
-    soup=BeautifulSoup(res.text, "lxml") # html 문서를 lxml을 통해서 BeautifulSoup 객체로 만들어준다.
-
-    dust=soup.find("div", attrs={"class":"tb_scroll"}).find("tbody").find("tr") # 도 별 정보
-    place=dust.find("th") # 도 이름
-    value=dust.find("span") # 미세먼지 값
-
-    print(dust.get_text())
-    print(place.get_text())
-    print(value.get_text())
-
-def scrape_naver_chungbuk(location):
-    print("[네이버 충청북도 미세먼지 지수]")
+def scrape_naver(location): # 네이버 미세먼지
     url="https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=blQ3&qvt=0&query=%EC%B6%A9%EB%B6%81%20%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80"
-    res=requests.get(url)
-    res.raise_for_status()
-    soup=BeautifulSoup(res.text, "lxml")  
+    # 스크랩 해올 주소
+    res=requests.get(url) # 주소를 받아온다
+    res.raise_for_status() # 문제 발견 시 프로그램 종료
+    soup=BeautifulSoup(res.text, "lxml")  # html 문서(DevTools)를 lxml을 통해서 BeautifulSoup 객체로 만들어준다.
 
-    dust=soup.find("div", attrs={"class":"map_area ct16"})
-    place=dust.find_all("span", attrs={"class":"cityname"})
-    value=dust.find_all("span", attrs={"class":"value"}) #.find("em")
+    dust=soup.find("div", attrs={"class":"map_area ct16"}) # 미세먼지 농도
+    place=dust.find_all("span", attrs={"class":"cityname"}) # 지역
+    value=dust.find_all("span", attrs={"class":"value"}) # 상태
 
     x=0
-    search = location
-    #search=input("도시 이름 : ") # 도시 찾기
-    for i in range(0, len(place)):
-        if search in place[i]:
+    for i in range(0, len(place)): # 해당 도시에 해당하는 값 찾기
+        if location in place[i]:
             x=i
 
-    print(place[x].get_text()) # 도시 이름
-    print("미세먼지 :", value[x].get_text()) # 미세먼지 값
-    if int(value[x].get_text()) <= 30:
+    print(place[x].get_text()) # 도시 이름 출력
+    print("미세먼지 :", value[x].get_text()) # 미세먼지 농도 출력
+    if int(value[x].get_text()) <= 30: # 상태 출력
         state = "좋음"
     elif int(value[x].get_text()) <= 80:
         state = "보통"
@@ -47,15 +29,14 @@ def scrape_naver_chungbuk(location):
     elif int(value[x].get_text()) >= 151:
         state = "매우나쁨"
 
-    message = [
+    message = [ # html로 띄울 값
         place[x].get_text(),
         value[x].get_text(),
         state
     ]
     return message
 
-def scrape_naver_25():
-    print("[네이버 충청북도 초미세먼지 지수]")
+def scrape_naver_25(): # 네이버 초미세먼지
     url="https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%B6%A9%EB%B6%81%EC%B4%88%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80"
     res=requests.get(url)
     res.raise_for_status()
@@ -89,6 +70,8 @@ def scrape_naver_25():
     ]
     return '<br>'.join(message)
 
+a=0
+count=0
 
 def weatheri(n):
     url="https://www.weatheri.co.kr/special/special05_1.php?a=6"
@@ -97,94 +80,89 @@ def weatheri(n):
     soup=BeautifulSoup(res.content.decode('utf-8','replace'), "lxml") # 글자 깨짐 해결
 
     dust=soup.find("table", attrs={"width":"100%", "border":"0", "cellpadding":"1", "cellspacing":"1", "bgcolor":"#D2D4D4"}).find_all("tr", attrs={"valign":"top", "bgcolor":"#FFFFFF", "height":"19"})
+    value=dust[n].find_all("td", attrs={"width":"7%", "align":"right"})[0].get_text()
+    global count
+    count+=1
+    if(value in "-\xa0"):
+        value = 0
+        count-=1
+    global a
+    a+=int(value)
 
-    #place=dust[n].find("td", attrs={"align":"center"}).get_text() #도시 이름
-    value=dust[n].find_all("td", attrs={"width":"7%", "align":"right"})[0].get_text() #미세먼지 값
-    #value2=dust[n].find_all("td", attrs={"width":"7%", "align":"right"})[1].get_text() #초미세먼지 값
-    #print(place)
+def scrape_weatheri(location):
+    value=0
+    if "청주" in location:
+        weatheri(1)
+        weatheri(11)
+        weatheri(12)
+        weatheri(13)
+        weatheri(14)
+        weatheri(19)
+        weatheri(20)
+        weatheri(22)
+        weatheri(23)
+        value = int(a/count)
+    elif "괴산" in location:
+        weatheri(2)
+        weatheri(3)
+        weatheri(29)
+        value = int(a/count)
+    elif "음성" in location:
+        weatheri(4)
+        weatheri(16)
+        weatheri(24)
+        value = int(a/count)
+    elif "단양" in location:
+        weatheri(5)
+        weatheri(6)
+        weatheri(9)
+        value = int(a/count)
+    elif "진천" in location:
+        weatheri(7)
+        weatheri(28)
+        value = int(a/count)
+    elif "증평" in location:
+        weatheri(8)
+        weatheri(27)
+        value = int(a/count)
+    elif "보은" in location:
+        weatheri(10)
+        value = a
+    elif "충주" in location:
+        weatheri(15)
+        weatheri(16)
+        weatheri(31)
+        weatheri(32)
+        value = int(a/count)
+    elif "영동" in location:
+        weatheri(17)
+        weatheri(33)
+        value = int(a/count)
+    elif "화성" in location:
+        weatheri(18)
+        value = a
+    elif "옥천" in location:
+        weatheri(21)
+        value = a
+    elif "제천" in location:
+        weatheri(25)
+        weatheri(30)
+        value = int(a/count)
 
-    return int(value)
-    #print("미세먼지 :", value)
+    state = "좋음"
+    if(15<value<30):
+        state="보통"
+    elif(value<75):
+        state="나쁨"
+    elif(value>=76):
+        state="매우나쁨"
 
-    # if int(value) <= 30:
-    #     print("좋음")
-    # elif int(value) <= 80:
-    #     print("보통")
-    # elif int(value) <= 150:
-    #         print("나쁨")
-    # elif int(value) >= 151:
-    #     print("매우나쁨")
-    # if "-" not in value2:
-    #     print("초미세먼지 :", value2)
-    #     if int(value2) <= 15:
-    #         print("좋음")
-    #     elif int(value2) <= 35:
-    #         print("보통")
-    #     elif int(value2) <= 75:
-    #         print("나쁨")
-    #     elif int(value2) >= 76:
-    #         print("매우나쁨")
-
-def scrape_weatheri_chungbuk():
-    search=input("도시 이름 : ")
-    a=0
-    if "청주" in search:
-        a+=weatheri(1)
-        a+=weatheri(11)
-        a+=weatheri(12)
-        a+=weatheri(13)
-        a+=weatheri(14)
-        a+=weatheri(19)
-        a+=weatheri(20)
-        a+=weatheri(22)
-        a+=weatheri(23)
-        return int(a/9)
-    elif "괴산" in search:
-        a+=weatheri(2)
-        a+=weatheri(3)
-        a+=weatheri(29)
-        return int(a/3)
-    elif "음성" in search:
-        a+=weatheri(4)
-        a+=weatheri(16)
-        a+=weatheri(24)
-        return int(a/3)
-    elif "단양" in search:
-        a+=weatheri(5)
-        a+=weatheri(6)
-        a+=weatheri(9)
-        return int(a/3)
-    elif "진천" in search:
-        a+=weatheri(7)
-        a+=weatheri(28)
-        return int(a/2)
-    elif "증평" in search:
-        a+=weatheri(8)
-        a+=weatheri(27)
-        return int(a/9)
-    elif "보은" in search:
-        a+=weatheri(10)
-        return a
-    elif "충주" in search:
-        a+=weatheri(15)
-        a+=weatheri(16)
-        a+=weatheri(31)
-        a+=weatheri(32)
-        return int(a/4)
-    elif "영동" in search:
-        a+=weatheri(17)
-        a+=weatheri(33)
-        return int(a/2)
-    elif "화성" in search:
-        a+=weatheri(18)
-        return a
-    elif "옥천" in search:
-        a+=weatheri(21)
-        return a
-    elif "제천" in search:
-        a+=weatheri(25)
-        a+=weatheri(30)
-        return int(a/2)
+    message = [
+        location,
+        value,
+        state
+    ]
+    return message
 
 
 def scrape_health():
@@ -234,12 +212,6 @@ def scrape_health():
     return int(result/count)
 
 if __name__ == "__main__":
-    #scrape_naver_chungbuk()
-    #scrape_naver_25()
-    #print(scrape_weatheri_chungbuk())
-    #print(scrape_health())
-    #scrape_health()
-
     app = Flask(__name__)
 
     @app.route('/')
@@ -250,9 +222,10 @@ if __name__ == "__main__":
         back = f"img/Cheongju.png"
         return render_template('site.html', photo1=photo1, photo2=photo2, photo3=photo3, back=back)
     
-    @app.route("/apply")
+    @app.route("/cheongju")
     def asdf():
-        result=scrape_naver_chungbuk("청주")
+        #result=scrape_naver("청주")
+        result=scrape_weatheri("청주")
         return render_template('Cheongju.html', result=result)
 
     if __name__ == '__main__':
