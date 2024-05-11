5eeed38ea46aa3a8051d63fde7311a50c4143ecf
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-05-11 12:01:14 +0900

    누락되었던 아날로그값을 본래값으로 변환하는 코드 추가

--- read_Dust.ino
+++ read_Dust.ino
@@ -38,19 +38,20 @@ float read_dust() {
   digitalWrite(PIN_DUST_LED, LOW); // LOW-> 센서 킴, 데이터 샘플링 시작
   delayMicroseconds(280);   // 0.28ms 동안 데이터 수집
   int dust_analog = analogRead(PIN_DUST_OUT);
-  float defautV = 0.3;
+  float dustV = dust_analog * (5.0 / 1023.0); //아날로그값을 본래 전압값으로 변환
+  float defautV = 0.3; // 미세먼지 기본 전압값 측정필요, 현재는 명시된 초기값의 평균으로 설정
   delayMicroseconds(40); // 0.32의 펄스를 유지해야해서 0.04 ms동안 대기
   digitalWrite(PIN_DUST_LED, HIGH); // 데이터 샘플링 종료, 센서 끔
   delayMicroseconds(9680); // 센서 안정화
-  float dustval = (dust_analog - defautV)/0.005; // 미세먼지 변수 기본값 측정 필요(기본 전압 일단 0.3으로 해둠)
+  float dustval = (dustV - defautV)/0.005; // 미세먼지 변수 기본값 측정 필요(기본 전압 일단 0.3으로 해둠)
   return dustval;
 }
 
 void print_Serial_LCD() {
-  float dust = read_dust();                  // 미세먼지 데이터 수집
+  float dust_ug = read_dust();                  // 미세먼지 데이터 수집
   
-  Serial.print(" Dust: ");
-  Serial.print(dust);
+  Serial.print(" 미세먼지: ");
+  Serial.print(dust_ug);
   Serial.println(" ug");       
 
   lcd.clear();  
