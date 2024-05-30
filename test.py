40791b10b4209671df3c5fd5433f2a17642d30d5
choigowon <cape326@chungbuk.ac.kr>
2024-05-30 11:06:34 +0900

    function

--- test.py
+++ test.py
@@ -3,9 +3,21 @@ from scr import Scr
 #print(Scr.scrape_naver("청주"))
 #print(Scr.arduino_data)
 
-class TestScr:
-    def test_arduino(self):
-        self.assertEqual(Scr.scrape_naver("청주"),19)
+class TestScr(unittest.TestCase):
+    # def test_arduino(self):
+    #     Scr.read_arduino()
+    def test_naver(self):
+        Scr.scrape_naver("청주")
+    def test_wea(self):
+        Scr.weatheri(1)
+    def test_wheatheri(self):
+        Scr.scrape_weatheri("청주")
+    def test_health(self):
+        Scr.scrape_health("청주")
+    def test_state(self):
+        Scr.state(100)
+    def test_suggest(self):
+        Scr.suggest(10,20,30)
 
 if __name__ == '__main__':
     unittest.main()
