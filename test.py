c3f01e03c53c3e8cb725f6918be6cc248e1b3d5b
choigowon <cape326@chungbuk.ac.kr>
2024-06-04 22:43:26 +0900

    coverage

--- test.py
+++ test.py
@@ -1,22 +1,35 @@
 import unittest
-from scr import Scr
+import qwer
 
 class TestScr(unittest.TestCase):
-    # def test_arduino(self):
-    #     Scr.read_arduino()
     def test_naver(self):
-        self.assertTrue(Scr.scrape_naver("청주"))
-        Scr.scrape_naver("청주")
+        qwer.scrape_naver("청주")
     def test_wea(self):
-        Scr.weatheri(1)
+        qwer.weatheri(1)
     def test_wheatheri(self):
-        Scr.scrape_weatheri("청주")
+        qwer.scrape_weatheri("청주")
+        qwer.scrape_weatheri("괴산")
+        qwer.scrape_weatheri("음성")
+        qwer.scrape_weatheri("단양")
+        qwer.scrape_weatheri("진천")
+        qwer.scrape_weatheri("증평")
+        qwer.scrape_weatheri("보은")
+        qwer.scrape_weatheri("충주")
+        qwer.scrape_weatheri("영동")
+        qwer.scrape_weatheri("화성")
+        qwer.scrape_weatheri("옥천")
+        qwer.scrape_weatheri("제천")
     def test_health(self):
-        Scr.scrape_health("청주")
+        qwer.scrape_health("청주")
     def test_state(self):
-        Scr.state(100)
+        qwer.state(20)
+        qwer.state(40)
+        qwer.state(60)
+        qwer.state(80)
+        qwer.state(100)
+        qwer.state(110)
     def test_suggest(self):
-        Scr.suggest(10,20,30)
+        qwer.suggest(10,20,30)
 
 if __name__ == '__main__':
     unittest.main()
